58b665b63d105cb19db964abb6cebb8c4e909fe6
Diana Dovhun <dianadovgun4@gmail.com>
2025-01-21 23:13:53 +0200

    Add 'Is Prime' exercise

---
+++ Exercise6/Exercise6.py
@@ -0,0 +1,8 @@
+def is_prime(number):
+    if number > 1 and number / 1:
+        print("True")
+    else:
+        print("False")
+
+user_input = int(input("Write your number: "))
+is_prime(user_input)
