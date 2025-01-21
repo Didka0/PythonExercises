f5a4c14d85503e53cb41db3c3dc3cfe09a226012
Diana Dovhun <dianadovgun4@gmail.com>
2025-01-21 22:42:08 +0200

    Zero/Positive/Negative exercise

---
+++ Exercise5/Exercise5.py
@@ -0,0 +1,8 @@
+number = int (input("Write your number: "))
+
+if number == 0:
+    print("Your number is: Zero")
+if number > 0:
+    print("Your number is: Positive")
+if number < 0:
+    print("Your number is: Negative")
