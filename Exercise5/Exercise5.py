4d145eb43b6ffa3e26100709a4c0dcc5a4eb7b60
Diana Dovhun <dianadovgun4@gmail.com>
2025-01-21 23:24:58 +0200

    Check number exercise update

--- Exercise5/Exercise5.py
+++ Exercise5/Exercise5.py
@@ -1,8 +1,10 @@
-number = int (input("Write your number: "))
+def check_number(number):
+    if number == 0:
+        print("Your number is: Zero")
+    if number > 0:
+        print("Your number is: Positive")
+    if number < 0:
+        print("Your number is: Negative")
 
-if number == 0:
-    print("Your number is: Zero")
-if number > 0:
-    print("Your number is: Positive")
-if number < 0:
-    print("Your number is: Negative")
+user_input = int(input("Write your number: "))
+check_number(user_input)
