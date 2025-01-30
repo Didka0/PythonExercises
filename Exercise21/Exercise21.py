06f225f938156474f4a0a6dc5b4fcea7cd25bc18
Diana Dovhun <dianadovgun4@gmail.com>
2025-01-30 13:54:15 +0200

    Print even exercise

---
+++ Exercise21/Exercise21.py
@@ -0,0 +1,15 @@
+def print_even():
+    for x in range (0, 100):
+        if x % 2 == 0:
+            print(x)
+
+print_even()
+
+print()
+
+def print_odd():
+    for x in range(0, 100):
+        if x % 2 != 0:
+            print(x)
+
+print_odd()
