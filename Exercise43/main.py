b91fac7a47446fe09aa382d927c1e70b067dbdc4
Diana Dovhun <dianadovgun4@gmail.com>
2025-03-25 20:53:20 +0200

    Updated version

--- Exercise43/main.py
+++ Exercise43/main.py
@@ -1,15 +1,19 @@
-name = input("What's your name? ")
+import colorama
+from colorama import Fore
+
+
+name = input("What's your name? ").capitalize()
 print("Hello,", name)
 
 answer = input("Do you want to calculate your BMI? ").lower()
 if answer =="yes":
     pass
-    print("Please enter your height like this: 1.60","\nOr your results will be invalid.")
+    print( Fore.YELLOW + "\nPlease enter your height like this: 1.60","\nOr your results will be invalid." + Fore.RESET )
 elif answer == "no":
     print("See you later!")
     exit
 else:
-    print("Invalid input")
+    print(Fore.RED + "Invalid input" + Fore.RESET )
 
 def BMI_calculator():
     weight = int(input("\nWhat's your weight? "))
@@ -17,6 +21,6 @@ def BMI_calculator():
 
     BMI = weight / (height * height)
     BMI = round(BMI, 2)
-    print("Your BMI is: ",BMI)
+    print(Fore.LIGHTMAGENTA_EX + "Your BMI is: ",BMI)
 
 BMI_calculator()
